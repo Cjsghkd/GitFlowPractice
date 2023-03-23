6334a0bea8ac9ae1752c2fb0dbfaddef89717f3b
박성현 <84944098+Cjsghkd@users.noreply.github.com>
2023-03-23 20:54:26 +0900

    Test Log add

--- app/src/main/java/com/debattle/gitflowpractice/MainActivity.kt
+++ app/src/main/java/com/debattle/gitflowpractice/MainActivity.kt
@@ -2,10 +2,13 @@ package com.debattle.gitflowpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("test", "develop1")
     }
 }
